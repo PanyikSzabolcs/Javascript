af8a2f1a06c96632b84cb8aa0eb80992b8bfe2ba
PanyikSzabolcs <66682951+PanyikSzabolcs@users.noreply.github.com>
2022-10-05 09:58:04 +0200

    Add files via upload

---
+++ változók/script.js
@@ -0,0 +1,37 @@
+
+// komment (line comment)
+
+/*
+blokk
+komment
+*/
+
+/*
+* Adattipusok
+*/
+
+// string (karakterfüzér)
+console.log('szia');
+console.log("szia");
+
+// tegyük nagybetűssé
+// nézzük meg hány karakterből áll
+// fordítsuk (reverse)
+// fűzzünk hozzá egy másik stringet
+
+// number (szám)
+
+console.log(500);
+
+// adjunk hozzá egy másik számot
+// vonjunk ki belőle
+// osztás utáni maradék
+
+// boolean (igaz hamis értéket reprezentáló típus)
+
+console.log(true);
+console.log(false);
+
+// logikai ÉS
+// (boolean, boolean) -> boolean
+
