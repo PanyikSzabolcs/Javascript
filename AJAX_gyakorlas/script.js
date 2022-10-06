7860d855c413056b85807481b8f7f0f54dba34cd
PanyikSzabolcs <66682951+PanyikSzabolcs@users.noreply.github.com>
2022-10-06 14:20:35 +0200

    Add files via upload

--- AJAX_gyakorlas/script.js
+++ AJAX_gyakorlas/script.js
@@ -1,48 +1,66 @@
-let state = {
-    teendok: [],
-    isPending: false,
-  };
-  
-  // 1. Kattintás eseményre köss be funkcionalitást
-  document.getElementById("fetch-btn").addEventListener("click", () => {
-    // 2. A esemény bekövetkezésekor állítsd a state isPending értékét true-ra
-    state.isPending = true;
-    render();
-  
-    // 3. Küldj AJAX kérést a beépített "fetch" függvény segítségével
-    // 4. A válaszként kapott adatokat szűrd meg a "filter" függvénnyel
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.json())
-      .then((todos) => todos.filter((todo) => todo.completed))
-      .then((todos) => {
-        // 5. A megszűrt adatokat írd be a state teendok kulcsa alá
-        state.teendok = todos;
-        // 6. Ezután állítsd vissza az isPending-et false-ra
-        state.isPending = false;
-        render();
-      })
-      .catch(console.log);
-  
-  });
-  
-  /* 7. Készíts egy renderelő függvényt, ami
-    - Ha az isPending true, akkor egy "Betöltés folyamatban" feliratot ír ki
-    - Ha az isPending false, akkor pedig kirajzolja az összes teendőt, 
-      ami a state-ben van
-  */
-  function render() {
-    if (state.isPending) {
-      document.getElementById("todo-list").innerHTML = "Betöltés folyamatban...";
-      return;
+/*
+ * AJAX kérések
+ * Asynchronous JavaScript and XML
+ */
+
+document.getElementById("login").onclick = function() {
+  var url = 'https://reqres.in/api/login';
+  var body = JSON.stringify({
+    email: 'eve.holt@reqres.in',
+    password: 'cityslicka'
+  })
+
+  // fetch
+  sendRequest2(url, 'POST', body)
+    // Promise<string>
+    // Promise<any>
+    .then(function(response) {
+      console.log(response);
+      // return "teszt";
+      return sendRequest2('https://reqres.in/api/users', 'GET', null);
+    })
+    .then(function(jovobeliErtek) {
+      console.log(jovobeliErtek);
+      return Promise.reject('Hiba oka');
+      // return sendRequest2('https://reqres.in/tovabbiEroforras1', 'GET', null);
+    })
+    .then(function (jovobeliErtek) {
+      console.log(jovobeliErtek);
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+};
+
+
+function sendRequest(url, method, body, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
     }
-  
-    document.getElementById("todo-list").innerHTML = state.teendok
-      .map((teendo, i) => 
-        `<span class="badge bg-primary me-1 mb-1">${i + 1}. ${teendo.title}</span>`
-      )
-      .join("");
-  
+  };
+  xhr.open(method, url);
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.send(body);
+}
+
+// Promise<_>
+
+function sendRequest2(url, method, body) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          if(xhr.status >= 200 && xhr.status < 300) {
+            resolve(JSON.parse(xhr.responseText));
+          } else {
+            reject(xhr.responseText);
+          }
+        }
+      };
+      xhr.open(method, url);
+      xhr.setRequestHeader('content-type', 'application/json');
+      xhr.send(body);
+    });
   }
-  
-  // 8. Hívd meg a renderelő függvényt a 2. és az 4. pont után is
-  
